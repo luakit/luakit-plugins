5d4ac8ac94b5b10d30ce2f6374236ad20f6603b9
Jasper den Ouden <o.jasper@gmail.com>
2015-01-28 13:17:20 +0100

    Added command to start (the renamed)`vid_function`, noticed that an argument was missing for the cclive default

--- use_video_program/init.lua
+++ use_video_program/init.lua
@@ -1,13 +1,14 @@
 -- (https://github.com/mason-larobina/luakit/wiki/Play-embedded-video-in-external-player)
 ----------------------------------------------------------------
 
-local key, buf, but = lousy.bind.key, lousy.bind.buf, lousy.bind.but
+local key, buf, cmd = lousy.bind.key, lousy.bind.buf, lousy.bind.cmd
 
 local config = globals.use_video_program or {}
 
 local use_mod = config.use_mod or {}
 local use_key = config.use_key or "v"
 local use_command = config.use_command or nil
+local vid_cmd = config.vid_cmd or "vid"
 
 local geometry = config.geometry or "1366x768"
    
@@ -21,28 +22,32 @@ local watch_functions = {
          luakit.spawn(string.format("mpv --geometry=%s %s", geometry, uri, finish))
       end
    end,
-   cclive = function(view, uri)
+   cclive = function(view, uri, finish)
       luakit.spawn(string.format("urxvt -e cclive --stream best --filename-format '%%t.%%s' "
                .. "--output-dir %q --exec='mplayer \"%%f\"' %q", os.getenv("HOME") .."/downloads", uri), finish)
    end
 }
 
-local use_function = config.use_function or watch_functions[which] or watch_function.mpv
+local vid_function = config.vid_function or watch_functions[which] or watch_function.mpv
 
-function activate (w)
+local function endprompt() w:set_prompt("-- VIDEO ENDED --") end
+
+local function activate (w)
    local uri = w.view.hovered_uri or w.view.uri
    if uri then
-      use_function(w.view, uri,
-                   function() w:set_prompt("-- VIDEO ENDED --") end)
+      vid_function(w.view, uri, endprompt)
    end
 end
 
 if use_key and not (use_key == "not") then
-   add_binds("normal", { key(use_mod, use_key, "View video external program",
-                             function(w) activate(w) end), })
+   add_binds("normal", { key(use_mod, use_key, "View video external program", activate) })
 end
 
 if use_command and not (use_command == "not") then
-   add_binds("normal", { buf(use_command, "View video external program",
-                             function(w) activate(w) end), })
+   add_binds("normal", { buf(use_command, "View video external program", activate) })
+end
+
+if vid_cmd then
+   add_cmds({ cmd(vid_cmd, "Use external video program on given URI",
+                  function(w,query) vid_function(w.view, query, endprompt) end) })
 end
