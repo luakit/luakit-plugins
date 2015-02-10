b6db03e2931cf8b3ad685d7046bc10712eaede8f
Jasper den Ouden <o.jasper@gmail.com>
2015-02-10 22:25:10 +0100

    Added watching chains of videos/listening to chains of audio. Havent been able to figure out how to demand the thing does not pop-up when switching. Note: perhaps some programs can handle the list themselves... But at some point we have to leave it up to that.

--- use_video_program/init.lua
+++ use_video_program/init.lua
@@ -8,7 +8,17 @@ local config = globals.use_video_program or {}
 local use_mod = config.use_mod or {}
 local use_key = config.use_key or "v"
 local use_command = config.use_command or nil
+
 local vid_cmd = config.vid_cmd or "vid"
+local vidlist_cmd = config.vid_cmd or "vidlist"
+
+local chain = config.chain
+if chain == nil then chain = true end
+
+-- Whether or not to pop up the window.
+local popup_initial = config.popup_initial  -- First in list.
+if popup_initial == nil then popup_initial = true end
+local popup = config.popup or false
 
 local geometry = config.geometry or "1366x768"
 
@@ -19,14 +29,16 @@ local geometry = config.geometry or "1366x768"
 local which = config.which or "mpv"  -- Allows some pre-defined uses.
 
 local watch_functions = {
-   mpv = function(view, uri, finish)
+   mpv = function(view, uri, finish, pop)
+      -- TODO it doesnt listen to `pop`
       if geometry == "fullscreen" then
          luakit.spawn(string.format("mpv --force-window --fs %s", uri), finish)
       else
-         luakit.spawn(string.format("mpv --force-window --geometry=%s %s", geometry, uri, finish))
+         luakit.spawn(string.format("mpv --force-window --geometry=%s %s", geometry, uri),
+                      finish)
       end
    end,
-   cclive = function(view, uri, finish)
+   cclive = function(view, uri, finish, pop)
       luakit.spawn(string.format("urxvt -e cclive --stream best --filename-format '%%t.%%s' "
                .. "--output-dir %q --exec='mplayer \"%%f\"' %q", os.getenv("HOME") .."/downloads", uri), finish)
    end
@@ -34,24 +46,49 @@ local watch_functions = {
 
 local vid_function = config.vid_function or watch_functions[which] or watch_function.mpv
 
-local function endprompt() w:set_prompt("-- VIDEO ENDED --") end
+local function endprompt(w) return function() w:set_prompt("-- VIDEO ENDED --") end end
 
-local function activate (w)
+local chain_sequence = {}
+
+local function next_in_chain(w, at_end)
+   return function()
+      table.remove(chain_sequence, 1)
+      if #chain_sequence > 0 then
+         vid_function(w.view, chain_sequence[1], next_in_chain(w, at_end, popup))
+      end
+   end
+end
+
+local function bind_fun(w)
    local uri = w.view.hovered_uri or w.view.uri
    if uri then
-      vid_function(w.view, uri, endprompt)
+      if chain then
+         table.insert(chain_sequence, uri)
+         if (#chain_sequence) == 1 then -- One left, get it started.
+            vid_function(w.view, uri, next_in_chain(w), popup_initial)
+         end
+      else
+         vid_function(w.view, uri, endprompt(w), popup_initial)
+      end
    end
 end
 
 if use_key and not (use_key == "not") then
-   add_binds("normal", { key(use_mod, use_key, "View video external program", activate) })
+   add_binds("normal", { key(use_mod, use_key, "View video external program", bind_fun) })
 end
 
 if use_command and not (use_command == "not") then
-   add_binds("normal", { buf(use_command, "View video external program", activate) })
+   add_binds("normal", { buf(use_command, "View video external program", bind_fun) })
 end
 
 if vid_cmd then
    add_cmds({ cmd(vid_cmd, "Use external video program on given URI",
                   function(w,query) vid_function(w.view, query, endprompt) end) })
+end
+
+if vidlist_cmd then
+   add_cmds({ cmd(vidlist_cmd, "List currently queued video/audio URIs",
+                  function(w,query)
+                     w:set_prompt(table.concat(chain_sequence, ",\n"))
+                  end) })
 end
