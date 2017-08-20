b34109c7cd88fb988d7a88ae557b78ebbfa37391
schvabodka-man <scvhapps@gmail.com>
2017-08-20 18:51:10 +0300

    fixed tabmenu

--- tabmenu/init.lua
+++ tabmenu/init.lua
@@ -7,6 +7,13 @@ local ipairs = ipairs
 local table = table
 
 local lousy = require "lousy"
+
+local modes = require "modes"
+
+local add_binds = modes.add_binds
+local add_cmds = modes.add_cmds
+local new_mode = modes.new_mode
+
 local add_binds, add_cmds = add_binds, add_cmds
 local new_mode, menu_binds = new_mode, menu_binds
 
@@ -14,73 +21,57 @@ module("plugins.tabmenu")
 
 hide_box = false
 
-local cmd = lousy.bind.cmd
 add_cmds({
-    cmd("tabmenu", function (w) w:set_mode("tabmenu") end),
+	  { ":tabmenu", [[Open tab menu]], function (w) w:set_mode("tabmenu") end },
 })
 
 local escape = lousy.util.escape
 new_mode("tabmenu", {
-    enter = function (w)
-        hide_box = not w.sbar.ebox.visible
-        local rows = {}
-        for _, view in ipairs(w.tabs.children) do
-            table.insert(rows, {escape(view.uri), escape(view.title), v = view })
-        end
-        w.menu:build(rows)
-        local cur = w.tabs:current()
-        local ind = 0
-        repeat w.menu:move_down(); ind = ind + 1 until ind == cur
-        w.sbar.ebox:show()
-        w:notify("Use j/k to move, d close, Return switch.", false)
-    end,
+			enter = function (w)
+			   hide_box = not w.sbar.ebox.visible
+			   local rows = {}
+			   for _, view in ipairs(w.tabs.children) do
+				  table.insert(rows, {escape(view.uri), escape(view.title), v = view })
+			   end
+			   w.menu:build(rows)
+			   local cur = w.tabs:current()
+			   local ind = 0
+			   repeat w.menu:move_down(); ind = ind + 1 until ind == cur
+			   w.sbar.ebox:show()
+			   w:notify("Del - close, Return - switch.", false)
+			end,
 
-    leave = function (w)
-        if hide_box == true then
-            w.sbar.ebox:hide()
-        end
-        w.menu:hide()
-    end,
+			leave = function (w)
+			   if hide_box == true then
+				  w.sbar.ebox:hide()
+			   end
+			   w.menu:hide()
+			end,
 })
 
-local key = lousy.bind.key
-add_binds("tabmenu", lousy.util.table.join({
-    -- Close tab
-    key({}, "d", function (w)
-        local row = w.menu:get()
-        if row and row.v then
-            local cur = w.view
-            w:close_tab(w.tabs[w.tabs:indexof(row.v)])
-            if cur ~= row.v then
-                w.menu:del()
-            else
-                w:set_mode()
-            end
-        end
-    end),
-
-    -- Switch to tab
-    key({}, "Return", function (w)
-        local row = w.menu:get()
-        if row and row.v then
-            local cur = w.view
-            if cur ~= row.v then
-                w.tabs:switch((w.tabs:indexof(row.v)))
-            else
-                w:set_mode()
-            end
-        end
-    end),
-
-    -- Exit menu
-    key({}, "`", function (w) w:set_mode() end),
-
-}, menu_binds))
-
--- Add key binds.
-local buf = lousy.bind.buf
-add_binds("normal", {
-    key({},     "m",    function (w)
-        w:set_mode("tabmenu")
-    end),
+add_binds("tabmenu", {
+			 { "<Delete>", "Delete tab.", function (w)
+				  local row = w.menu:get()
+				  if row and row.v then
+					 local cur = w.view
+					 w:close_tab(w.tabs[w.tabs:indexof(row.v)])
+					 if cur ~= row.v then
+						w.menu:del()
+					 else
+						w:set_mode()
+					 end
+				  end
+			 end },
+			 { "<Return>", "Open tab.", function (w)
+					  local row = w.menu:get()
+					  if row and row.v then
+						 local cur = w.view
+						 if cur ~= row.v then
+							w.tabs:switch((w.tabs:indexof(row.v)))
+						 else
+							w:set_mode()
+						 end
+					  end
+				end },
 })
+
