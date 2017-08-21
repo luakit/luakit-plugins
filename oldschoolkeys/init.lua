0baa3d601b7509438eab9156abee158fdfe76d01
schvabodka-man <scvhapps@gmail.com>
2017-08-21 16:01:21 +0300

    fixed old school keybindings

--- oldschoolkeys/init.lua
+++ oldschoolkeys/init.lua
@@ -4,24 +4,16 @@
 
 local pairs = pairs
 local lousy = require("lousy")
-local key, buf, but = lousy.bind.key, lousy.bind.buf, lousy.bind.but
-local add_binds, add_cmds = add_binds, add_cmds
 
-module("oldschoolkeys")
+local modes = require "modes"
 
-local userbindings = {
-    -- Normal mode:
-    normal = {
-        key({},          "b",           function (w, m) w:back(m.count)    end, {count=1}),
-        key({"Mod1"},    "Page_Up",     function (w, m) w.tabs:reorder(w.view, w.tabs:current() - m.count) end, {count=1}),
-        key({"Mod1"},    "Page_Down",   function (w, m) w.tabs:reorder(w.view, (w.tabs:current() + m.count) % w.tabs:count()) end, {count=1}),
-    },
-}
+local add_binds = modes.add_binds
 
-function load()
-    for modename, modebinds in pairs(userbindings) do
-        add_binds(modename, modebinds)
-    end
-end
+module("plugins.oldschoolkeys")
 
-load()
+add_binds("normal",
+		  {{ "b", "Back", function (w, m) w:back(m.count) end, {count=1}},
+			 { "<Mod1-Page_Up>", "Reorder tabs", function (w, m) w.tabs:reorder(w.view, w.tabs:current() - m.count) end, {count=1}},
+			 { "<Mod1-Page_Down>", "Reorder tabs", function (w, m) w.tabs:reorder(w.view, (w.tabs:current() + m.count) % w.tabs:count()) end, {count=1}}})
+
+
