bdf61a4e3aaad943254ecd8afc4e6be4640ca842
schvabodka-man <scvhapps@gmail.com>
2017-08-21 16:01:21 +0300

    Fix oldschoolkeys plugin

--- oldschoolkeys/init.lua
+++ oldschoolkeys/init.lua
@@ -1,27 +1,24 @@
 ------------------------------------------------------------------------
 -- Add some convenient keybindings back.                              --
 ------------------------------------------------------------------------
+local modes = require "modes"
 
-local pairs = pairs
-local lousy = require("lousy")
-local key, buf, but = lousy.bind.key, lousy.bind.buf, lousy.bind.but
-local add_binds, add_cmds = add_binds, add_cmds
+local add_binds = modes.add_binds
 
-module("oldschoolkeys")
+local _M = {}
 
-local userbindings = {
-    -- Normal mode:
-    normal = {
-        key({},          "b",           function (w, m) w:back(m.count)    end, {count=1}),
-        key({"Mod1"},    "Page_Up",     function (w, m) w.tabs:reorder(w.view, w.tabs:current() - m.count) end, {count=1}),
-        key({"Mod1"},    "Page_Down",   function (w, m) w.tabs:reorder(w.view, (w.tabs:current() + m.count) % w.tabs:count()) end, {count=1}),
-    },
-}
+add_binds("normal",{
+    { "b", "Back", function (w, m) w:back(m.count) end, {count=1}},
+    { "<Mod1-Page_Up>", "Reorder tabs",
+      function (w, m)
+         w.tabs:reorder(w.view, w.tabs:current() - m.count)
+      end, {count=1}},
+    { "<Mod1-Page_Down>", "Reorder tabs",
+      function (w, m)
+         w.tabs:reorder(w.view, (w.tabs:current() + m.count) % w.tabs:count())
+      end, {count=1}}
+})
 
-function load()
-    for modename, modebinds in pairs(userbindings) do
-        add_binds(modename, modebinds)
-    end
-end
+return _M
 
-load()
+-- vim: et:sw=4:ts=8:sts=4:tw=80
