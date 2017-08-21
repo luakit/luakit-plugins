ca487c839c2b603de80c7c2f71a26e4386065c85
schvabodka-man <scvhapps@gmail.com>
2017-08-21 15:45:54 +0300

    fixed yankselect

--- yanksel/init.lua
+++ yanksel/init.lua
@@ -5,18 +5,22 @@
 local error = error
 local luakit = luakit
 local lousy = require("lousy")
-local key, buf, but = lousy.bind.key, lousy.bind.buf
-local add_binds, add_cmds = add_binds, add_cmds
+
+local modes = require "modes"
+
+local add_binds = modes.add_binds
+local add_cmds = modes.add_cmds
 
 module("plugins.yanksel")
 
-add_binds("normal", {
-    buf("^ys$",
-	function (w)
-	    local text = luakit.selection.primary
-	    if not text then w:error("Empty selection.") return end
-	    luakit.selection.clipboard = text
-	    w:notify("Yanked selection: " .. text)
-	    luakit.selection.primary = ""
-	end),
-})
+function yank_select(w)
+   local text = luakit.selection.primary
+   if not text then w:error("Empty selection.") return end
+   luakit.selection.clipboard = text
+   w:notify("Yanked selection: " .. text)
+   luakit.selection.primary = ""		
+end
+
+add_binds("normal", {{ "^ys$", "Yank selection", function(w) yank_select(w) end}})
+
+add_cmds({{ ":yanksel", [[Yank selection]], function (w) yank_select(w) end },})
